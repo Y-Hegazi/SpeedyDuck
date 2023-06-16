88e44c336bbdf8fd7ad7b7430dbd4dadd5f92d1f
Yousef Hegazi <109203673+Y-Hegazi@users.noreply.github.com>
2023-06-16 09:18:24 +0300

    adding lidar node to real robot

--- speedy_duck/launch/launch_robot.launch.py
+++ speedy_duck/launch/launch_robot.launch.py
@@ -90,6 +90,19 @@ def generate_launch_description():
         condition=IfCondition(use_rviz)
     )
 
+    lidar = Node(
+            package='rplidar_ros',
+            node_executable='rplidar_composition',
+            node_name='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/ttyUSB0',
+                'serial_baudrate': 115200,  # A1 / A2
+                # 'serial_baudrate': 256000, # A3
+                'frame_id': 'laser_frame',
+                'inverted': False,
+                'angle_compensate': True,}])
+
 
 
     # Launch them all!
